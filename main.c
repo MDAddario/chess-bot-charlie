593d7ef5fb2bf413cba3c79ee5d7a3e8b1e7d4bc
MDAddario <38199062+MDAddario@users.noreply.github.com>
2019-05-27 17:44:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "chess_framework.h"
 
 // The main attraction!
@@ -13,8 +14,66 @@ int main(int argc, char* argv[]){
 	Board* board = (Board*)malloc(sizeof(Board));
 	BoardReset(board);
 
+	// Interactive chess playing
+	char input, buffer;
+	Move* move_list;
+	U16 length, move_num, castling_rights, EP_files;
+
 	/*
-	// FEN testing
+	Obvious move generation bug when playing the following sequence:
+	12 -> 0 -> 29 -> 5 ->
+	There are two ways to capture the d6 pawn?
+	Move 29, 30 breaks the game
+	-------------------------------------------
+	Clearly, there is an issue with EP, in at least one of
+
+		- psuedoMoveGenerator()
+		- validateMove()
+		- makeMove()
+		- undoMove()
+	*/
+
+	while(1){
+
+		BoardPrint(board);
+		printf("\n\nWould you like to makeMove (m) or undoMove (u): ");
+		input = getchar();
+		while ((buffer = getchar()) != '\n' && buffer != EOF) { }
+
+		if (input == 'm'){
+
+			move_list = legalMoveGenerator(global, board, &length, isInCheck(global, board, 64, YES));
+			movePrinter(global, board);
+			printf("Please input which move you would like to make: ");
+			scanf("%hu", &move_num);
+			while ((buffer = getchar()) != '\n' && buffer != EOF) { }
+
+			castling_rights = board->castlingRights;
+			EP_files = board->EPFiles;	
+			makeMove(global, board, move_list[move_num], NO);
+			
+		}
+		else if (input == 'u'){
+
+			if (board->ply == 1){
+				printf("You need to make a move first idiot.\n");
+				continue;
+			}
+
+			undoMove(global, board, move_list[move_num], castling_rights, EP_files);
+		}
+		else
+			printf("Please input 'm' or 'u' for make or undo.\n");
+
+	}
+
+	return 0;
+
+	/*
+	// EXAMPLE FEN LOADING
+	// RUN PROGRAM AS:
+	//	./main (FEN STRING)
+
 	if (argc < 5){
 		printf("Please provide a complete FEN string.\n");
 		return -1;
